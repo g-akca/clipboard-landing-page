8297c41a4ecec49ebac9d05394c04e52ad58ad58
g-akca <gok.akca66@gmail.com>
2026-03-30 16:31:41 +0300

    Add LogosSection structure and styles

--- src/components/LogosSection.jsx
+++ src/components/LogosSection.jsx
@@ -1,7 +1,18 @@
+import googleLogo from "/images/logo-google.png";
+import ibmLogo from "/images/logo-ibm.png";
+import microsoftLogo from "/images/logo-microsoft.png";
+import hpLogo from "/images/logo-hp.png";
+import vgLogo from "/images/logo-vector-graphics.png";
+
 function LogosSection() {
   return (
-    <>
-    </>
+    <section className="flex flex-col items-center gap-14">
+      <img src={googleLogo} alt="Google logo" className="w-34.75" />
+      <img src={ibmLogo} alt="IBM logo" className="w-30.25" />
+      <img src={microsoftLogo} alt="Microsoft logo" className="w-33.75" />
+      <img src={hpLogo} alt="Hewlett Packard logo" className="w-28.25" />
+      <img src={vgLogo} alt="Vector Graphics logo" className="w-26.25" />
+    </section>
   )
 }
 
