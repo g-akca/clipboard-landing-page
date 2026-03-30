a99fe276a9b270342e22aee5476bd7a78ddf6460
g-akca <gok.akca66@gmail.com>
2026-03-30 13:34:34 +0300

    Add AccessSection structure and styles

--- src/components/AccessSection.jsx
+++ src/components/AccessSection.jsx
@@ -1,7 +1,15 @@
+import devicesImg from "/images/image-devices.png";
+
 function AccessSection() {
   return (
-    <>
-    </>
+    <section className="flex flex-col gap-18 text-center">
+      <div className="flex flex-col gap-4">
+        <h2 className="font-semibold text-[28px] leading-[125%] tracking-[-0.5px] text-gray-700">Access Clipboard Anywhere</h2>
+        <p className="tracking-base">Whether you’re on the go, or at your computer, you can access all your Clipboard snippets in a few simple clicks.</p>
+      </div>
+      
+      <img src={devicesImg} alt="Image of a tablet and phone" />
+    </section>
   )
 }
 
