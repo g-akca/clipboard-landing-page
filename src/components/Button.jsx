f391da469d2211c24e6e503431bcd94a6e4fc1fd
g-akca <gok.akca66@gmail.com>
2026-03-31 11:43:00 +0300

    Add tablet responsive HeroSection and Button styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ function Button({ className, children }) {
   return (
     <button 
       type="button" 
-      className={`cursor-pointer w-full max-w-100 h-14 rounded-[28px] inset-shadow-[0_-3px_rgba(0,0,0,0.15)] 
+      className={`cursor-pointer w-full max-w-100 tablet:max-w-56.75 h-14 rounded-[28px] inset-shadow-[0_-3px_rgba(0,0,0,0.15)] 
       text-white font-semibold text-[20px] leading-base tracking-[0.5px] transition-all ${className}`}
     >
       {children}
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,16 +4,16 @@ import logo from "/images/logo.svg";
 
 function HeroSection() {
   return (
-    <section className="flex flex-col items-center gap-16">
+    <section className="flex flex-col items-center gap-16 tablet:gap-14">
       <img src={logo} alt="Logo image" className="w-31.25" />
 
-      <div className="flex flex-col gap-14">
+      <div className="flex flex-col gap-14 tablet:gap-12">
         <div className="flex flex-col gap-4 text-center">
-          <h1 className="font-semibold text-[32px] leading-[125%] tracking-[-0.5px] text-gray-700">A history of everything you copy</h1>
+          <h1 className="font-semibold text-[32px] tablet:text-[40px] leading-[125%] tracking-[-0.5px] tablet:tracking-[-0.75px] text-gray-700">A history of everything you copy</h1>
           <p className="tracking-base">Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.</p>
         </div>
 
-        <div className="flex flex-col gap-6 items-center">
+        <div className="flex flex-col gap-6 justify-center items-center tablet:flex-row tablet:gap-4">
           <Button className="bg-green-500 hover:bg-green-300">
             Download for iOS
           </Button>
