fca3d9523c7d1c33801be97de50140ecd95e1407
g-akca <gok.akca66@gmail.com>
2026-03-31 11:57:30 +0300

    Add tablet responsive ToolsSection styles

--- src/components/ToolsSection.jsx
+++ src/components/ToolsSection.jsx
@@ -4,37 +4,37 @@ import previewIcon from "/images/icon-preview.svg";
 
 function ToolsSection() {
   return (
-    <section className="flex flex-col gap-22 text-center">
+    <section className="flex flex-col gap-22 tablet:gap-18 text-center">
       <div className="flex flex-col gap-4">
-        <h2 className="font-semibold text-[28px] leading-[125%] tracking-[-0.5px] text-gray-700">Supercharge your workflow</h2>
+        <h2 className="font-semibold text-[28px] tablet:text-[36px] leading-[125%] tracking-[-0.5px] tablet:tracking-[-0.6px] text-gray-700">Supercharge your workflow</h2>
         <p className="tracking-base">We’ve got the tools to boost your productivity.</p>
       </div>
 
-      <div className="flex flex-col gap-16 items-center">
-        <div className="flex flex-col gap-10 items-center">
+      <div className="flex flex-col gap-16 tablet:gap-12 items-center">
+        <div className="flex flex-col gap-10 items-center max-w-98">
           <img src={blacklistIcon} alt="Blacklist icon" />
 
           <div className="flex flex-col gap-4">
             <h3 className="font-semibold text-[24px] leading-[125%] tracking-[-0.4px] text-gray-700">Create blacklists</h3>
-            <p className="tracking-base">Ensure sensitive information never makes its way to your clipboard by excluding certain sources.</p>
+            <p className="text-base leading-base tracking-base">Ensure sensitive information never makes its way to your clipboard by excluding certain sources.</p>
           </div>
         </div>
 
-        <div className="flex flex-col gap-10 items-center">
+        <div className="flex flex-col gap-10 items-center max-w-98">
           <img src={textIcon} alt="Text icon" />
 
           <div className="flex flex-col gap-4">
             <h3 className="font-semibold text-[24px] leading-[125%] tracking-[-0.4px] text-gray-700">Plain text snippets</h3>
-            <p className="tracking-base">Remove unwanted formatting from copied text for a consistent look.</p>
+            <p className="text-base leading-base tracking-base">Remove unwanted formatting from copied text for a consistent look.</p>
           </div>
         </div>
 
-        <div className="flex flex-col gap-10 items-center">
+        <div className="flex flex-col gap-10 items-center max-w-98">
           <img src={previewIcon} alt="Preview icon" />
 
           <div className="flex flex-col gap-4">
             <h3 className="font-semibold text-[24px] leading-[125%] tracking-[-0.4px] text-gray-700">Sneak preview</h3>
-            <p className="tracking-base">Quick preview of all snippets on your Clipboard for easy access.</p>
+            <p className="text-base leading-base tracking-base">Quick preview of all snippets on your Clipboard for easy access.</p>
           </div>
         </div>
       </div>
