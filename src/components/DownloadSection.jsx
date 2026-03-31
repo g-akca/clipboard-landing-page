d485b6bfb4b405d8a9d31c015b819cd32616d0a1
g-akca <gok.akca66@gmail.com>
2026-03-31 13:01:12 +0300

    Add desktop responsive DownloadSection and Footer styles

--- src/components/DownloadSection.jsx
+++ src/components/DownloadSection.jsx
@@ -2,13 +2,13 @@ import Button from "./Button";
 
 function DownloadSection() {
   return (
-    <section className="flex flex-col gap-14 tablet:gap-12 text-center">
+    <section className="flex flex-col gap-14 tablet:gap-12 text-center max-w-182.5">
       <div className="flex flex-col gap-4">
         <h2 className="font-semibold text-[28px] tablet:text-[36px] leading-[125%] tracking-[-0.5px] tablet:tracking-[-0.6px] text-gray-700">Clipboard for iOS and Mac OS</h2>
         <p className="tracking-base">Available for free on the App Store. Download for Mac or iOS, sync with iCloud and you’re ready to start adding to your clipboard.</p>
       </div>
 
-      <div className="flex flex-col gap-6 items-center tablet:flex-row tablet:gap-2 tablet:justify-center">
+      <div className="flex flex-col gap-6 items-center tablet:flex-row tablet:gap-4 tablet:justify-center">
         <Button className="bg-green-500 hover:bg-green-300">
           Download for iOS
         </Button>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,19 +6,21 @@ import InstagramIcon from "./icons/InstagramIcon";
 
 function Footer() {
   return (
-    <footer className="bg-gray-50 h-125 flex justify-center items-center">
-      <div className="flex flex-col gap-8 items-center">
-        <img src={logo} alt="Logo image" className="w-13.75" />
+    <footer className="bg-gray-50 h-125 flex justify-center items-center desktop:h-37.5 desktop:px-41">
+      <div className="flex flex-col gap-8 items-center desktop:flex-row desktop:justify-between desktop:w-full desktop:max-w-350">
+        <div className="flex flex-col gap-8 items-center desktop:flex-row desktop:justify-between desktop:w-167.5">
+          <img src={logo} alt="Logo image" className="w-13.75" />
 
-        <ul className="flex flex-col gap-6 text-center text-[20px] leading-base tracking-base text-gray-700">
-          <li><a href="#" className="hover:text-green-500 transition-all">FAQs</a></li>
-          <li><a href="#" className="hover:text-green-500 transition-all">Contact Us</a></li>
-          <li><a href="#" className="hover:text-green-500 transition-all">Privacy Policy</a></li>
-          <li><a href="#" className="hover:text-green-500 transition-all">Press Kit</a></li>
-          <li><a href="#" className="hover:text-green-500 transition-all">Install Guide</a></li>
-        </ul>
+          <ul className="flex flex-col gap-6 text-center text-[20px] leading-base tracking-base text-gray-700 desktop:h-17 desktop:text-start desktop:flex-wrap desktop:gap-x-16 desktop:gap-y-2">
+            <li><a href="#" className="hover:text-green-500 transition-all">FAQs</a></li>
+            <li><a href="#" className="hover:text-green-500 transition-all">Contact Us</a></li>
+            <li><a href="#" className="hover:text-green-500 transition-all">Privacy Policy</a></li>
+            <li><a href="#" className="hover:text-green-500 transition-all">Press Kit</a></li>
+            <li><a href="#" className="hover:text-green-500 transition-all">Install Guide</a></li>
+          </ul>
+        </div>
 
-        <div className="flex justify-center items-center gap-10">
+        <div className="flex justify-center items-center gap-10 desktop:gap-6">
           <FacebookIcon className="cursor-pointer transition-all text-gray-700 hover:text-green-500" />
           <TwitterIcon className="cursor-pointer transition-all text-gray-700 hover:text-green-500" />
           <InstagramIcon className="cursor-pointer transition-all text-gray-700 hover:text-green-500" />
