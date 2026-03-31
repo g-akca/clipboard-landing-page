1bcad02f6cf0b2f79e33ecff3a7774a6da007f51
g-akca <gok.akca66@gmail.com>
2026-03-31 11:52:42 +0300

    Add tablet responsive AccessSection styles

--- src/components/AccessSection.jsx
+++ src/components/AccessSection.jsx
@@ -2,9 +2,9 @@ import devicesImg from "/images/image-devices.png";
 
 function AccessSection() {
   return (
-    <section className="flex flex-col gap-18 text-center">
+    <section className="flex flex-col gap-18 tablet:gap-20 text-center">
       <div className="flex flex-col gap-4">
-        <h2 className="font-semibold text-[28px] leading-[125%] tracking-[-0.5px] text-gray-700">Access Clipboard Anywhere</h2>
+        <h2 className="font-semibold text-[28px] tablet:text-[36px] leading-[125%] tracking-[-0.5px] tablet:tracking-[-0.6px] text-gray-700">Access Clipboard Anywhere</h2>
         <p className="tracking-base">Whether you’re on the go, or at your computer, you can access all your Clipboard snippets in a few simple clicks.</p>
       </div>
       
