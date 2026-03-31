8c55b6eb40b8728a21e4a15aa597270aa8b13c02
g-akca <gok.akca66@gmail.com>
2026-03-31 12:01:09 +0300

    Add tablet responsive DownloadSection styles

--- src/components/DownloadSection.jsx
+++ src/components/DownloadSection.jsx
@@ -2,13 +2,13 @@ import Button from "./Button";
 
 function DownloadSection() {
   return (
-    <section className="flex flex-col gap-14 text-center">
+    <section className="flex flex-col gap-14 tablet:gap-12 text-center">
       <div className="flex flex-col gap-4">
-        <h2 className="font-semibold text-[28px] leading-[125%] tracking-[-0.5px] text-gray-700">Clipboard for iOS<br/>and Mac OS</h2>
+        <h2 className="font-semibold text-[28px] tablet:text-[36px] leading-[125%] tracking-[-0.5px] tablet:tracking-[-0.6px] text-gray-700">Clipboard for iOS and Mac OS</h2>
         <p className="tracking-base">Available for free on the App Store. Download for Mac or iOS, sync with iCloud and you’re ready to start adding to your clipboard.</p>
       </div>
 
-      <div className="flex flex-col gap-6 items-center">
+      <div className="flex flex-col gap-6 items-center tablet:flex-row tablet:gap-2 tablet:justify-center">
         <Button className="bg-green-500 hover:bg-green-300">
           Download for iOS
         </Button>
